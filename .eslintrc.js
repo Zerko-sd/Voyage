242cc6e0bcacbef43ef550666c249f780ba7f65e
Nitish <Nitish88631006@gmail.com>
2025-11-19 15:52:52 +0530

    Simplify ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-  root: true,
   extends: ["next/core-web-vitals"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-  },
 };
