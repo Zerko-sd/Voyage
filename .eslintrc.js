c880e1b475d0d56c467e288800ac71b3b3b9cdcc
Nitish <Nitish88631006@gmail.com>
2025-11-19 15:50:11 +0530

    Fix ESLint configuration for TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
   extends: ["next/core-web-vitals"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
